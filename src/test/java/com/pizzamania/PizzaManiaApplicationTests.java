e9abe5ebd950f8e2a16b6e3bbf51337a6bac4f94
Amit Vishwakarma <amitvishwa789@gmail.com>
2026-09-22 13:52:14 +0530

    Fix isolated application context tests

--- src/test/java/com/pizzamania/PizzaManiaApplicationTests.java
+++ src/test/java/com/pizzamania/PizzaManiaApplicationTests.java
@@ -2,9 +2,25 @@ package com.pizzamania;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.pizzamania.security.repository.RoleRepository;
+import com.pizzamania.security.repository.UserRepository;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PizzaManiaApplicationTests {
+
+	@MockBean
+	private JwtDecoder jwtDecoder;
+
+	@MockBean
+	private RoleRepository roleRepository;
+
+	@MockBean
+	private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
